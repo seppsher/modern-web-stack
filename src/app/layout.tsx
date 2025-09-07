1ff79c48aaa50467ee6031f37ee7280aaf65a24b
seppsher <lukasz.kowalskii@interia.pl>
2025-09-07 12:21:34 +0200

    Next.js migration

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "../index.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      </head>
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
